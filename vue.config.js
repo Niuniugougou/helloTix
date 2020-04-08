f0c57d34eba9ec31335c879a69d06d01749f2337
ltx <490419614@qq.com>
2020-04-08 13:59:17 +0800

    修改配置信息，package插件版本使用小版本最新

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ process.env.VUE_APP_VERSION = require('./package.json').version
 process.env.VUE_APP_BUILD_TIME = require('dayjs')().format('YYYY-M-D HH:mm:ss')
 
 // 基础路径 注意发布之前要先修改这里
-let publicPath = '/'
+let publicPath = './'
 
 module.exports = {
   publicPath, // 根据你的实际情况更改这里
@@ -64,7 +64,9 @@ module.exports = {
                   drop_debugger: true,
                   pure_funcs: ['console.log']
                 }
-              }
+              },          
+              sourceMap: false,
+              parallel: true,
             })
           ])
       })
